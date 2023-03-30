1994a3ae03adc50dfd42654570941400ce98168d
EmilioVS <6360@holbertonstudents.com>
2023-03-29 19:14:03 -0700

    Ejercicio 0

--- _printf.c
+++ _printf.c
@@ -1,25 +1,44 @@
 #include "main.h"
 /**
-* _printf - prints formatted data
-* @format: string
-* Return: count
-*/
+* _printf - main function to print in console
+* @format: array to print and check type
+* Return: count of character printed
+**/
 int _printf(const char *format, ...)
 {
-int count = 0;
-va_list args;
-convert_t funcs[] = {
-{"c", print_char},
-{"s", print_string},
-{"%", print_percent},
-{"d", print_int},
-{"i", print_int},
-{NULL, NULL}
-};
-if (format == NULL)    
+int count = -1;
+if (format != NULL)
+{
+int i;
+va_list values;
+int (*savef)(va_list);
+va_start(values, format);
+if (format[0] == '%' && format[1] == '\0')
 return (-1);
-va_start(args, format);
-count = print(format, funcs, args);
-va_end(args);
+count = 0;
+for (i = 0; format[i] != '\0'; i++)
+{
+if (format[i] == '%')
+{
+if (format[i + 1] == '%')
+{
+count = count + _putchar(format[i]);
+i++;
+}
+else if (format[i + 1] != '\0')
+{
+savef = get_func(format[i + 1]);
+if (savef)
+count = count + savef(values);
+else
+count += _putchar(format[i]) + _putchar(format[i + 1]);
+i++;
+}
+}
+else
+count = count + _putchar(format[i]);
+}
+va_end(values);
+}
 return (count);
 }
